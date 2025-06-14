f7feaf2cf16a459e702d85fcc47e7866b136ed76
Diego Ramos Sanchez <127451062+AlexanderRamosSanchez@users.noreply.github.com>
2025-06-14 03:00:55 +0000

    [LOW] : Correccion de paquetes

--- src/main/java/pe/edu/vallegrande/information/config/SecurityConfig.java
+++ src/main/java/pe/edu/vallegrande/information/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package pe.edu.vallegrande.database.config;
+package pe.edu.vallegrande.information.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

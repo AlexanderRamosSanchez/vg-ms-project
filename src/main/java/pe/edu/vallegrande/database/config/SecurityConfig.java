6103c178217dc53086ea7f2056c9ccb6b030f8c2
Diego Ramos Sanchez <127451062+AlexanderRamosSanchez@users.noreply.github.com>
2025-07-04 10:19:47 -0500

    [MEDIUM] : Security Microservices Family

--- src/main/java/pe/edu/vallegrande/database/config/SecurityConfig.java
+++ src/main/java/pe/edu/vallegrande/database/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
 
                         // API endpoints con autenticación
                         // GET: accesible por USER y ADMIN
-                        .pathMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER")
+                        .pathMatchers(HttpMethod.GET, "/api/**").permitAll()
 
                         // POST, PUT, DELETE: solo ADMIN
                         .pathMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
